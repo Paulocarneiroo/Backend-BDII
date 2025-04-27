3f4d239a442c91704c53d52a61b0bfbe593635d7
rodrigonbjrz <rodrigonbjrz@gmail.com>
2025-04-26 23:12:52 -0300

    Add - Calculation Controller

---
+++ src/main/java/com/bdII/bdII/controllers/CalcController.java
@@ -0,0 +1,42 @@
+package com.bdII.bdII.controllers;
+
+import com.bdII.bdII.entities.Calc;
+import com.bdII.bdII.services.CalcService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/calcs")
+public class CalcController {
+
+    @Autowired
+    private CalcService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Calc>> findAllCalculationsByUserId(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findAllCalculationsByUserId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Calc> insertCalc(@RequestBody Calc calc) {
+        calc = service.save(calc);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(calc.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(calc);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAllCalculationsByUserId(@PathVariable Long id) {
+        service.deleteAllCalculationsByUserId(id);
+        return ResponseEntity.noContent().build();
+    }
+}
