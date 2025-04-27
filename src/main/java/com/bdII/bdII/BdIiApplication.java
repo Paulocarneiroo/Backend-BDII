1c1a640cdaa32b8782cff84f88f937452df5c56e
Paulocarneiroo <paulocarneirosegundo@gmail.com>
2025-04-26 21:02:29 -0300

    projeto inicializado

---
+++ src/main/java/com/bdII/bdII/BdIiApplication.java
@@ -0,0 +1,13 @@
+package com.bdII.bdII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdII/bdII/BdIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdII.bdII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
